bc83cffd0cbfb6ac3a145bf7ee1f117c7630467e
老潘orz <wasdpkj@hotmail.com>
2015-07-02 01:43:38 +0800

    Revert "microduino"
    This reverts commit cfc61862ab0c8c0643d9e5a1b480c601b5452657.

--- arduino-ide-Support/[1.0.x]-hardware&library/libraries/_03_Microduino_CoreRF_ZMAC/vendor_boardtypes.h
+++
@@ -1,7 +0,0 @@
-
-/*
- * Zigduino
- * http://www.logos-electro.com/zigduino
- */
- 
-#define ZIGDUINO                (0xa1)
--- arduino-ide-Support/[1.6.x]-hardware(library)/hardware/Microduino/avr/libraries/_07_Sensor_Tem_LM75/README.md
+++
@@ -1,2 +0,0 @@
-# lm75
-Arduino library for access to LM75 temperature sensor
