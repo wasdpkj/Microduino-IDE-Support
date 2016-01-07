b172cd5f9a99102c6abf694274cd9156762036b3
老潘orz <wasdpkj@hotmail.com>
2016-01-07 19:18:39 +0800

    updata sd library

--- arduino-ide-Support/[1.6.x]-hardware(library)/hardware/Microduino/avr/libraries/_05_Microduino_SD/examples/Datalogger/Datalogger.ino
+++ arduino-ide-Support/[1.6.x]-hardware(library)/hardware/Microduino/avr/libraries/_05_Microduino_SD/examples/Datalogger/Datalogger.ino
@@ -1,46 +1,41 @@
 /*
   SD card datalogger
- 
- This example shows how to log data from three analog sensors 
+
+ This example shows how to log data from three analog sensors
  to an SD card using the SD library.
- 	
+
  The circuit:
  * analog sensors on analog ins 0, 1, and 2
  * SD card attached to SPI bus as follows:
  ** MOSI - pin 11
  ** MISO - pin 12
  ** CLK - pin 13
- ** CS - pin 7
- 
+ ** CS - pin 4
+
  created  24 Nov 2010
  modified 9 Apr 2012
  by Tom Igoe
- 
+
  This example code is in the public domain.
- 	 
+
  */
 
+#include <SPI.h>
 #include <SD.h>
 
-// On the Ethernet Shield, CS is pin 4. Note that even if it's not
-// used as the CS pin, the hardware CS pin (10 on most Arduino boards,
-// 53 on the Mega) must be left as an output or the SD library
-// functions will not work.
-// Microduino SD shield: pin 7
+// Microduino SD module: pin 7
 const int chipSelect = 7;
 
-void setup()
-{
- // Open serial communications and wait for port to open:
-  Serial.begin(115200);
-   while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
   }
 
 
   Serial.print("Initializing SD card...");
-  // make sure that the default chip select pin is set to
-  
+
   // see if the card is present and can be initialized:
   if (!SD.begin(chipSelect)) {
     Serial.println("Card failed, or not present");
@@ -50,8 +45,7 @@ void setup()
   Serial.println("card initialized.");
 }
 
-void loop()
-{
+void loop() {
   // make a string for assembling the data to log:
   String dataString = "";
 
@@ -60,7 +54,7 @@ void loop()
     int sensor = analogRead(analogPin);
     dataString += String(sensor);
     if (analogPin < 2) {
-      dataString += ","; 
+      dataString += ",";
     }
   }
 
@@ -74,11 +68,11 @@ void loop()
     dataFile.close();
     // print to the serial port too:
     Serial.println(dataString);
-  }  
+  }
   // if the file isn't open, pop up an error:
   else {
     Serial.println("error opening datalog.txt");
-  } 
+  }
 }
 
 
