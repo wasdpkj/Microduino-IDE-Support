48bf1231723d11cbb17e61702c29afe3ae989f66
老潘orz <wasdpkj@hotmail.com>
2016-01-20 12:10:26 +0800

    updata Multi for dot matrix

---
+++ arduino-ide-Support/[1.6.x]-hardware(library)/hardware/Microduino/avr/libraries/_01_Microduino_DotMatrix/examples/Multi/Multi.ino
@@ -0,0 +1,77 @@
+#include <Wire.h>
+#include "Microduino_Matrix.h"
+
+#define ADDR1 64
+LedControl MatrixA = LedControl(ADDR1);
+#define ADDR2 63
+LedControl MatrixB = LedControl(ADDR2);
+
+void setup() { // initalizes and sets up the initial values. Declaring function setup.
+  Serial.begin(9600); // setting data rate as 9600 bits per second for serial data communication to computer
+  //  delay(6000);
+  Serial.println("Setup"); //prints data to serial port as human-readable text
+  Wire.begin();
+
+  MatrixA.clearDisplay(); //clear the display after each letter
+  MatrixB.clearDisplay(); //clear the display after each letter
+
+  MatrixA.setColor(0, 255, 255); //clear the display after each letter
+  MatrixB.setColor(255, 0, 255); //clear the display after each letter
+
+  for (int a = 0; a < 64; a++)
+  {
+    MatrixA.setCursor(a, 0);
+    MatrixB.setCursor(a, 0);
+    MatrixA.print("HELLO mCookie");
+    MatrixB.print("HELLO mCookie");
+    delay(50);
+  }
+  MatrixB.clearDisplay(); //clear the display after each letter
+}
+
+void loop() { //declaring function loop
+  for (int y = 0; y < 8; y++) {
+    for (int x = 0; x < 8; x++) {
+      randomSeed(analogRead(A0));
+      MatrixA.setLedColor(x, y, random(0, 255), random(0, 255), random(0, 255));
+      MatrixB.setLedColor(x, y, random(0, 255), random(0, 255), random(0, 255));
+      delay(20);
+    }
+  }
+  delay(2000);
+
+  MatrixA.clearDisplay(); //clear the display after each letter
+  MatrixB.clearDisplay(); //clear the display after each letter
+  MatrixA.setColor(255, 0, 0); //clear the display after each letter
+  MatrixB.setColor(255, 0, 0); //clear the display after each letter
+  MatrixA.writeString(20, "RED");
+  MatrixB.writeString(20, "RED");
+  delay(500);
+
+  MatrixA.clearDisplay(); //clear the display after each letter
+  MatrixB.clearDisplay(); //clear the display after each letter
+  MatrixA.setColor(0, 255, 0); //clear the display after each letter
+  MatrixB.setColor(0, 255, 0); //clear the display after each letter
+  MatrixA.writeString(20, "GREEN");
+  MatrixB.writeString(20, "GREEN");
+  delay(500);
+
+  MatrixA.clearDisplay(); //clear the display after each letter
+  MatrixB.clearDisplay(); //clear the display after each letter
+  MatrixA.setColor(0, 0, 255); //clear the display after each letter
+  MatrixB.setColor(0, 0, 255); //clear the display after each letter
+  MatrixA.writeString(20, "BLUE");
+  MatrixB.writeString(20, "BLUE");
+  delay(500);
+
+  MatrixA.clearDisplay(); //clear the display after each letter
+  MatrixB.clearDisplay(); //clear the display after each letter
+  MatrixA.clearColor(); //clear the display after each letter
+  MatrixB.clearColor(); //clear the display after each letter
+  MatrixA.writeString(20, "Null");
+  MatrixB.writeString(20, "Null");
+  delay(500);
+
+  MatrixA.clearDisplay();
+  MatrixB.clearDisplay();
+}
