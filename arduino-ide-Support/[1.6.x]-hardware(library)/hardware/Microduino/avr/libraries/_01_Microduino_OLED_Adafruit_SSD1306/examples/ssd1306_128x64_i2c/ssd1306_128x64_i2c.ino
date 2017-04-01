bd9b46dfcd8ed1450ba84d30fb9a3f9561f3da17
老潘orz <wasdpkj@hotmail.com>
2017-04-01 15:36:24 +0800

    fix example

--- arduino-ide-Support/[1.6.x]-hardware(library)/hardware/Microduino/avr/libraries/_01_Microduino_OLED_Adafruit_SSD1306/examples/ssd1306_128x64_i2c/ssd1306_128x64_i2c.ino
+++ arduino-ide-Support/[1.6.x]-hardware(library)/hardware/Microduino/avr/libraries/_01_Microduino_OLED_Adafruit_SSD1306/examples/ssd1306_128x64_i2c/ssd1306_128x64_i2c.ino
@@ -21,7 +21,7 @@ All text above, and the splash screen must be included in any redistribution
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-#define OLED_RESET 4
+#define OLED_RESET -1
 Adafruit_SSD1306 display(OLED_RESET);
 
 #define NUMFLAKES 10
