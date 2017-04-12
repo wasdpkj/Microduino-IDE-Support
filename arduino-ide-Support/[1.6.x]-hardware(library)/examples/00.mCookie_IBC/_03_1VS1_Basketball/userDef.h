4eef4b1ab7a6101c4e8d172f2d32c4ab5388550f
老潘orz <wasdpkj@hotmail.com>
2017-04-12 21:19:42 +0800

    更新IBC的example

--- arduino-ide-Support/[1.6.x]-hardware(library)/examples/00.mCookie_IBC/_03_1VS1_Basketball/userDef.h
+++ arduino-ide-Support/[1.6.x]-hardware(library)/examples/00.mCookie_IBC/_03_1VS1_Basketball/userDef.h
@@ -4,7 +4,7 @@
 #define PIN_LED      12      //ColorLED pin.
 #define PIN_LINEA    A0      //Line Finder A pin.
 #define PIN_LINEB    A2      //Line Finder B pin.
-#define LED_NUM      2 
+#define LED_NUM      2
 
 //=================================DO IT YOURSELF: CHANGE THE CODE BELOW!========================================//
 //Example: Change "#define BRIGHT_MAX 128" to "#define BRIGHT_MAX 180".                                          //
@@ -15,8 +15,11 @@
 #define BRIGHT_MAX   128     //Max LED brightness. Max brightness is 255. Minimum brightness is 0.
 
 #define DOCK_MAX     500     //Line Finder minimum activation value. Increasing this means you will have to use a
-                             //darker-colored ball/item to activate the Line Finder.
-                             
+//darker-colored ball/item to activate the Line Finder.
+
 #define SCORE_MAX    5       //Number of points you have to be ahead by to win! The default is 5 points, meaning 
-                             //there will only be a winner if that person is beating the other person by 5 points.
-                             //Lower this number to have shorter games. 
+//there will only be a winner if that person is beating the other person by 5 points.
+//Lower this number to have shorter games.
+
+#define leftColor   6     //左灯颜色
+#define rightColor   9    //右灯颜色
