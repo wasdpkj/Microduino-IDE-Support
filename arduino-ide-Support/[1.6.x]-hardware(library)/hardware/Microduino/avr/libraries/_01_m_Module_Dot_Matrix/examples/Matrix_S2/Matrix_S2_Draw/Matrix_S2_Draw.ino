8cd0b066fe4a69f1622232f6580af39934f98030
老潘orz <wasdpkj@hotmail.com>
2017-07-15 00:08:47 +0800

    精简点阵examples申明

--- arduino-ide-Support/[1.6.x]-hardware(library)/hardware/Microduino/avr/libraries/_01_m_Module_Dot_Matrix/examples/Matrix_S2/Matrix_S2_Draw/Matrix_S2_Draw.ino
+++ arduino-ide-Support/[1.6.x]-hardware(library)/hardware/Microduino/avr/libraries/_01_m_Module_Dot_Matrix/examples/Matrix_S2/Matrix_S2_Draw/Matrix_S2_Draw.ino
@@ -5,27 +5,6 @@
 // @老潘orz  wasdpkj@hotmail.com
 // ==============
 
-// Microduino-IDE
-// ==============
-// Microduino Getting start:
-// http://www.microduino.cc/download/
-
-// Microduino IDE Support：
-// https://github.com/wasdpkj/Microduino-IDE-Support/
-
-// ==============
-// Microduino wiki:
-// http://wiki.microduino.cc
-
-// ==============
-// E-mail:
-// Kejia Pan
-// pankejia@microduino.cc
-
-// ==============
-// Weibo:
-// @老潘orz
-
 #include <Microduino_Matrix.h>
 
 uint8_t Addr[MatrixPix_X][MatrixPix_Y] = {  //1x1
