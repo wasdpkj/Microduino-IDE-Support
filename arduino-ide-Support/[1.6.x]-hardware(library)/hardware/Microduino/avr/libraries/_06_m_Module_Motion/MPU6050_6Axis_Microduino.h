c751dbb3ad1337c3a165795a13ce14bc2d0c20ea
老潘orz <wasdpkj@hotmail.com>
2017-07-26 17:46:35 +0800

    6050库结构调整

--- arduino-ide-Support/[1.6.x]-hardware(library)/hardware/Microduino/avr/libraries/_06_m_Module_Motion/MPU6050_6Axis_Microduino.h
+++ arduino-ide-Support/[1.6.x]-hardware(library)/hardware/Microduino/avr/libraries/_06_m_Module_Motion/MPU6050_6Axis_Microduino.h
@@ -33,8 +33,8 @@ THE SOFTWARE.
 #define _MPU6050_6AXIS_MICRODUINO_H_
 
 #include <I2Cdev.h>
-#include "helper_3dmath.h"
-#include "imu.h"
+#include "./imu/helper_3dmath.h"
+#include "./imu/imu.h"
 
 // MotionApps 2.0 DMP implementation, built using the MPU-6050EVB evaluation board
 #define MPU6050_INCLUDE_DMP_MOTIONAPPS20
