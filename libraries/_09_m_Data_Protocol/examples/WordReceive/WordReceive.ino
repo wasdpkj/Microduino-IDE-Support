39da41f78403a452197ec6e9bf3677eeb2a225f4
老潘orz <wasdpkj@hotmail.com>
2017-09-21 01:32:15 +0800

    更新通讯库example以及keyword，WordReceive支持ESP32

--- libraries/_09_m_Data_Protocol/examples/WordReceive/WordReceive.ino
+++ libraries/_09_m_Data_Protocol/examples/WordReceive/WordReceive.ino
@@ -6,11 +6,20 @@
 
 #include <Microduino_Protocol.h>
 
+#if defined (__AVR__)
 SoftwareSerial mySerial1(4, 5);
 SoftwareSerial mySerial2(2, 3);
+ProtocolSer protocolA(&mySerial1, 16);  //采用mySerial1，数据长度为16个字节
+ProtocolSer protocolB(&mySerial2, 16);  //采用mySerial2，数据长度为16个字节
+#endif
 
-ProtocolSer protocolA(&mySerial1, 16);	//采用mySerial1，数据长度为16个字节
-ProtocolSer protocolB(&mySerial2, 16);   //mySerial2，数据长度为16个字节
+#if defined (ESP32)
+HardwareSerial mySerial1(1);  //UART1
+HardwareSerial mySerial2(2);  //UART2
+ProtocolSer protocolA(&mySerial1, 16, D4, D5);  //采用mySerial1，数据长度为16个字节
+ProtocolSer protocolB(&mySerial2, 16, D2, D3);  //采用mySerial2，数据长度为16个字节
+#endif
+
 
 uint16_t sendData[8] = {1500, 1500, 1500, 1500, 1000, 1000, 1000, 1000};
 uint16_t recData[8];
@@ -25,26 +34,26 @@ void setup() {
 }
 
 void loop() {
-	
+  
   if(millis() - sendTime > 1000)
   {
-	  sendTime = millis();
-	  protocolA.write(0x01, (uint8_t *)sendData, 16);
+    sendTime = millis();
+    protocolA.write(0x01, (uint8_t *)sendData, 16);
     Serial.println("protocolA send !");
   }
   
   if(protocolB.available())
   {
-	  protocolB.readWords(&recCmd, recData, 8);
-	  Serial.println("protocolB Received !");
-	  Serial.print("recCmd: ");
-	  Serial.print(recCmd);
-	  Serial.print("  Data:");
-	  for(uint8_t i=0; i<8; i++)
-	  {
-		  Serial.print(" ");
-		  Serial.print(recData[i]);
-	  }
+    protocolB.readWords(&recCmd, recData, 8);
+    Serial.println("protocolB Received !");
+    Serial.print("recCmd: ");
+    Serial.print(recCmd);
+    Serial.print("  Data:");
+    for(uint8_t i=0; i<8; i++)
+    {
+      Serial.print(" ");
+      Serial.print(recData[i]);
+    }
     Serial.println();
   }
  
