46fa5e43677b8189503b90fb64d1db47a74a16b6
cuiwenjing <cuiwenjing@microduino.cc>
2018-01-30 16:08:00 +0800

    使统一后的电机方向与此前core控制的方向一致

--- libraries/_04_m_Module_Motor/Microduino_Motor.h
+++ libraries/_04_m_Module_Motor/Microduino_Motor.h
@@ -21,25 +21,25 @@
 #include <Arduino.h>
 
 #if defined(__AVR_ATmega32U4__) || (__AVR_ATmega1284P__) || defined(__AVR_ATmega644P__) || defined(__AVR_ATmega128RFA1__)
-#define MOTOR0_PINA 6  
-#define MOTOR0_PINB 8  //PWM
-#define MOTOR1_PINA 7  //PWM   
-#define MOTOR1_PINB 5
+#define MOTOR0_PINA 8  //PWM
+#define MOTOR0_PINB 6
+#define MOTOR1_PINA 5   
+#define MOTOR1_PINB 7  //PWM
 #else
-#define MOTOR0_PINA 6  //PWM
-#define MOTOR0_PINB 8  
-#define MOTOR1_PINA 7
-#define MOTOR1_PINB 5  //PWM  
+#define MOTOR0_PINA 8  
+#define MOTOR0_PINB 6  //PWM
+#define MOTOR1_PINA 5  //PWM
+#define MOTOR1_PINB 7
 #endif
 
 #define FREE    0   //释放
 #define SPEED_MAX 255
 
 #if defined (ESP32)
-#define MOTOR0_PINA D6  //PWM
-#define MOTOR0_PINB D8  //PWM
-#define MOTOR1_PINA D7  //PWM 
-#define MOTOR1_PINB D5  //PWM
+#define MOTOR0_PINA D8  //PWM
+#define MOTOR0_PINB D6  //PWM
+#define MOTOR1_PINA D5  //PWM 
+#define MOTOR1_PINB D7  //PWM
 #define LEDC_TIMER_8BIT   8
 #define LEDC_BASEFREQ     1000
 #define SPEED_MAX         256
