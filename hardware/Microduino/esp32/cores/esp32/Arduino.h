8b7e9786f1ad286c54121ae398a17e77b3a68442
DESKTOP-XPS15-P\wasdp <wasdpkj@hotmail.com>
2018-12-21 23:41:45 +0800

    修正ESP32支持包问题

--- hardware/Microduino/esp32/cores/esp32/Arduino.h
+++ hardware/Microduino/esp32/cores/esp32/Arduino.h
@@ -75,6 +75,7 @@
 
 #define abs(x) ((x)>0?(x):-(x))
 #define constrain(amt,low,high) ((amt)<(low)?(low):((amt)>(high)?(high):(amt)))
+#define round(x)     ((x)>=0?(long)((x)+0.5):(long)((x)-0.5))
 #define radians(deg) ((deg)*DEG_TO_RAD)
 #define degrees(rad) ((rad)*RAD_TO_DEG)
 #define sq(x) ((x)*(x))
@@ -183,6 +184,11 @@ extern "C" void configTzTime(const char* tz,
 // WMath prototypes
 long random(long);
 #endif /* __cplusplus */
+
+// arduino is not compatible with std::vector
+#define min(a,b) ((a)<(b)?(a):(b))
+#define max(a,b) ((a)>(b)?(a):(b))
+
 
 #define _min(a,b) ((a)<(b)?(a):(b))
 #define _max(a,b) ((a)>(b)?(a):(b))
